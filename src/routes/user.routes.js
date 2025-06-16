b00a032e71a6fef389e58854b0e93a3782683e5f
wagner1067 <barra-wag@hotmail.com>
2025-06-16 20:54:40 -0300

    Feat: Create Schema and middlewares

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import userController from "../controller/user.controllers.js";
+import { validate } from "../middlewares/validation.middlewares.js";
+import { userSchema } from "../schema/user.schema.js";
 
 const router = Router();
 
-router.post("/users", userController.createUserController);
+router.post(
+  "/users",
+  validate(userSchema),
+  userController.createUserController
+);
 
 export default router;
