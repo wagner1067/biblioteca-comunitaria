3dc3eb8f4ceb70df5f675bf4b7e8b8ab85fa912c
wagner1067 <barra-wag@hotmail.com>
2025-06-15 21:39:33 -0300

    Feat: Create Error Release

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,6 +1,10 @@
 import userRepositories from "../repositories/user.repositories.js";
 
 async function createUserService(newUser) {
+  const foundUser = await userRepositories.findUserByEmail(newUser.email);
+  if (foundUser) {
+    throw new Error("Usuário já existe com este email");
+  }
   const user = await userRepositories.createUserRepository(newUser);
   return user;
 }
